81a4b2255a521329b7788af9a59dce79a9981669
Sergey Royz <zjor.se@gmail.com>
2022-12-31 15:54:23 +0100

    frontend added

--- backend/deploy-with-helm.sh
+++ backend/deploy-with-helm.sh
@@ -6,4 +6,4 @@ VERSION=$(git rev-parse --short HEAD)
 
 set -x
 
-helm upgrade --namespace ${NS} --install ${APP} --set version=${VERSION} ./ops/me-webapp-bot
+helm upgrade --namespace ${NS} --create-namespace --install ${APP} --set version=${VERSION} ./ops/me-webapp-bot
